a4db7c365308fb294ba5d331229540d7e02ea73f
kaito3desuyo <admin@seapolis.me>
2019-03-17 16:05:40 +0900

    heroku対応？8

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import * as cookieParser from 'cookie-parser'
 import * as cors from 'cors'
 import indexRouter from './routes/index'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
