19d0d8e3ed2963cbbb0d0abffad8f1687ba2599b
kaito3desuyo <admin@seapolis.me>
2019-03-20 23:59:20 +0900

    Open ID Connect 実装準備

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as express from 'express'
 import * as cookieParser from 'cookie-parser'
 import * as cors from 'cors'
 import indexRouter from './routes/index'
+import axios from 'axios'
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -9,6 +11,51 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(cors())
+
+app.use(async (req, res, next) => {
+  console.log('Interceot')
+  // token取得
+  try {
+    const params = new URLSearchParams()
+    params.append('grant_type', 'client_credentials')
+    const token = await axios.post('http://localhost:3030/token', params, {
+      headers: {
+        Authorization: 'Basic ' + Buffer.from(`0:0`).toString('base64')
+      }
+    })
+    console.log(token.data.access_token)
+
+    const params2 = new URLSearchParams()
+    params2.append('token', '')
+    const introspection = await axios.post(
+      'http://localhost:3030/token/introspection',
+      params2,
+      {
+        headers: {
+          Authorization: 'Basic ' + Buffer.from(`0:0`).toString('base64')
+        }
+      }
+    )
+
+    console.log(introspection)
+
+    if (!introspection.data.active) {
+      throw {
+        response: {
+          data: {
+            error: 'invalid_request',
+            error_description: 'token has expired'
+          }
+        }
+      }
+    }
+
+    next()
+  } catch (err) {
+    console.log(err, 'エラー')
+    res.json(err.response.data)
+  }
+})
 
 app.use('/', indexRouter)
 
