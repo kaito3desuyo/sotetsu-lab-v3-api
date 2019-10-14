041127339fd5a8bfe4df1060b4a055ab6041e480
kaito3desuyo <admin@seapolis.me>
2019-10-14 16:13:15 +0900

    seed修正 サービス別駅一覧の並び方を修正

--- src/db/backup/1569142718194-SeedTrip20181208.ts
+++ src/db/backup/1569142718194-SeedTrip20181208.ts
@@ -12,7 +12,7 @@ export class SeedTrip201812081569142718194 implements MigrationInterface {
     const promises = data.map(seed => {
       return getRepository(TripBlock).save({ trips: [seed.trip] });
     });
-    const results = await Promise.all(promises);
+    const results: TripBlock[] = await Promise.all(promises);
 
     const tripOperationList = [];
     results.forEach(result => {
@@ -26,20 +26,16 @@ export class SeedTrip201812081569142718194 implements MigrationInterface {
             seed.trip.calendar_id === trip.calendar_id,
         ).trip_operation_list.operation_id;
 
-        const startTimeId = find(
-          trip.times,
-          time => time.arrival_time === null,
-        );
-        const endTimeId = find(
-          trip.times,
-          time => time.departure_time === null,
-        );
+        const startTime = find(trip.times, time => time.arrival_time === null);
+        const endTime = find(trip.times, time => time.departure_time === null);
 
         tripOperationList.push({
           trip_id: tripId,
           operation_id: operationId,
-          start_time_id: startTimeId,
-          end_time_id: endTimeId,
+          start_station_id: startTime.station_id,
+          end_station_id: endTime.station_id,
+          start_time_id: startTime.id,
+          end_time_id: endTime.id,
         });
       });
     });
