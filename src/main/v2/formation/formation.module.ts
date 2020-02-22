132cca9361775a33d88072ca080b641f2f0527a4
kaito3desuyo <admin@seapolis.me>
2020-02-22 19:36:11 +0900

    Guardをかける

--- src/main/v2/formation/formation.module.ts
+++ src/main/v2/formation/formation.module.ts
@@ -3,10 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Formation } from '../../../main/v1/formation/formation.entity';
 import { FormationService } from './formation.service';
 import { FormationController } from './formation.controller';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Formation])],
-  controllers: [FormationController],
-  providers: [FormationService],
+    imports: [TypeOrmModule.forFeature([Formation])],
+    controllers: [FormationController],
+    providers: [FormationService, AuthService],
 })
 export class FormationV2Module {}
--- src/main/v2/operation-sighting/operation-sighting.v2.module.ts
+++ src/main/v2/operation-sighting/operation-sighting.v2.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { OperationSightingV2Controller } from './operation-sighting.v2.controller';
 import { OperationSightingLibsModule } from '../../../libs/operation-sighting/operation-sighting.libs.module';
+import { AuthService } from '../../../shared/services/auth.service';
 
 @Module({
     imports: [OperationSightingLibsModule],
     controllers: [OperationSightingV2Controller],
-    providers: [],
+    providers: [AuthService],
 })
 export class OperationSightingV2Module {}
