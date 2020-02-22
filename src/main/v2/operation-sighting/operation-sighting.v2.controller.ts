35e679dbafa75b598fd4f78c47219cc977a4b58d
kaito3desuyo <admin@seapolis.me>
2020-02-22 19:36:11 +0900

    Guardをかける

--- src/main/v2/operation-sighting/operation-sighting.v2.controller.ts
+++ src/main/v2/operation-sighting/operation-sighting.v2.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { OperationSightingService } from '../../../libs/operation-sighting/application-service/operation-sighting.service';
+import { AuthGuard } from '../../../shared/guards/auth.guard';
 
 @Controller()
+@UseGuards(AuthGuard)
 export class OperationSightingV2Controller {
     constructor(private operationSightingService: OperationSightingService) {}
 
