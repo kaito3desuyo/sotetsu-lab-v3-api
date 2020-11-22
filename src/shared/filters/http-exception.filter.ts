8d986b92f511f491459fb40314adb9c093b39923
kaito3desuyo <admin@seapolis.me>
2020-11-23 02:25:38 +0900

    nestjsアップデート

--- src/shared/filters/http-exception.filter.ts
+++ src/shared/filters/http-exception.filter.ts
@@ -1,19 +1,27 @@
 import {
-  Catch,
-  HttpException,
-  ExceptionFilter,
-  ArgumentsHost,
+    Catch,
+    HttpException,
+    ExceptionFilter,
+    ArgumentsHost,
 } from '@nestjs/common';
 import { Response, Request } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
-    const ctx = host.switchToHttp();
-    const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
+    catch(exception: HttpException, host: ArgumentsHost) {
+        const ctx = host.switchToHttp();
+        const response = ctx.getResponse<Response>();
+        const request = ctx.getRequest<Request>();
+        const status = exception.getStatus();
 
+        response.status(status).json({
+            errors: {
+                statusCode: status,
+                message: exception.message,
+            },
+        });
+
+        /*
     if (typeof exception.message === 'string') {
       response.status(status).json({
         errors: {
@@ -21,12 +29,13 @@ export class HttpExceptionFilter implements ExceptionFilter {
           message: exception.message,
         },
       });
-    } else {
+    } else if (typeof exception.message === 'object') {
       response.status(status).json({
         errors: {
           ...exception.message,
         },
       });
     }
-  }
+    */
+    }
 }
