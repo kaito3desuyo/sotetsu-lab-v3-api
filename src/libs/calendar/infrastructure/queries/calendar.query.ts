c18a681af48d5479720285f38f60d08d66943da2
kaito3desuyo <admin@seapolis.me>
2021-09-12 18:54:34 +0900

    微修正

--- src/libs/calendar/infrastructure/queries/calendar.query.ts
+++ src/libs/calendar/infrastructure/queries/calendar.query.ts
@@ -3,10 +3,8 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { CrudRequest, GetManyDefaultResponse } from '@nestjsx/crud';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { isArray } from 'lodash';
-import { getDayOfWeek, isHoliday, isNewYear } from 'src/core/util/day-of-week';
-import { Brackets, Repository } from 'typeorm';
+import { Repository } from 'typeorm';
 import { CalendarDetailsDto } from '../../usecase/dtos/calendar-details.dto';
-import { CalendarFindManyBySpecificDateParam } from '../../usecase/params/calendar-find-many-by-specific-date.param';
 import { buildCalendarDetailsDto } from '../builders/calendar-dto.builder';
 import { CalendarModel } from '../models/calendar.model';
 
@@ -25,65 +23,6 @@ export class CalendarQuery extends TypeOrmCrudService<CalendarModel> {
         CalendarDetailsDto[] | GetManyDefaultResponse<CalendarDetailsDto>
     > {
         const models = await this.getMany(query);
-
-        if (isArray(models)) {
-            return models.map((o) => buildCalendarDetailsDto(o));
-        } else {
-            const data = models.data.map((o) => buildCalendarDetailsDto(o));
-            return {
-                ...models,
-                data,
-            };
-        }
-    }
-
-    async findManyCalendarsBySpecificDate(
-        query: CrudRequest,
-        params: CalendarFindManyBySpecificDateParam,
-    ): Promise<
-        CalendarDetailsDto[] | GetManyDefaultResponse<CalendarDetailsDto>
-    > {
-        const qb = await this.createBuilder(query.parsed, query.options, true);
-        const customQb = (query.parsed.filter.length === 0
-            ? qb.where(
-                  new Brackets((subqb) => {
-                      return subqb
-                          .where('start_date <= :startDate', {
-                              startDate: params.date,
-                          })
-                          .orWhere('start_date IS NULL');
-                  }),
-              )
-            : qb.andWhere(
-                  new Brackets((subqb) => {
-                      return subqb
-                          .where('start_date <= :startDate', {
-                              startDate: params.date,
-                          })
-                          .orWhere('start_date IS NULL');
-                  }),
-              )
-        )
-            .andWhere(
-                new Brackets((subqb) => {
-                    return subqb
-                        .where(':endDate <= end_date', {
-                            endDate: params.date,
-                        })
-                        .orWhere('end_date IS NULL');
-                }),
-            )
-            .andWhere(
-                isHoliday(params.date) || isNewYear(params.date)
-                    ? 'sunday = true'
-                    : `${getDayOfWeek(params.date)} = true`,
-            );
-
-        const models = await this.doGetMany(
-            customQb,
-            query.parsed,
-            query.options,
-        );
 
         if (isArray(models)) {
             return models.map((o) => buildCalendarDetailsDto(o));
--- src/libs/calendar/usecase/calendar.v2.service.ts
+++ src/libs/calendar/usecase/calendar.v2.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { CrudRequest, GetManyDefaultResponse } from '@nestjsx/crud';
+import { merge } from 'lodash';
+import { isHoliday, isNewYear, getDayOfWeek } from 'src/core/util/day-of-week';
 import { CalendarQuery } from '../infrastructure/queries/calendar.query';
 import { CalendarDetailsDto } from './dtos/calendar-details.dto';
 import { CalendarFindManyBySpecificDateParam } from './params/calendar-find-many-by-specific-date.param';
@@ -22,9 +24,38 @@ export class CalendarV2Service {
     ): Promise<
         CalendarDetailsDto[] | GetManyDefaultResponse<CalendarDetailsDto>
     > {
-        return this.calendarQuery.findManyCalendarsBySpecificDate(
-            query,
-            params,
+        return this.calendarQuery.findManyCalendars(
+            merge(query, {
+                parsed: {
+                    search: {
+                        $and: [
+                            {
+                                $or: [
+                                    {
+                                        startDate: { $lte: params.date },
+                                    },
+                                    {
+                                        startDate: null,
+                                    },
+                                ],
+                            },
+                            {
+                                $or: [
+                                    {
+                                        endDate: { $gte: params.date },
+                                    },
+                                    {
+                                        endDate: null,
+                                    },
+                                ],
+                            },
+                            isHoliday(params.date) || isNewYear(params.date)
+                                ? { sunday: true }
+                                : { [getDayOfWeek(params.date)]: true },
+                        ],
+                    },
+                },
+            }),
         );
     }
 
