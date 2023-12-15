aa3e098da7462eed6df407100c1e3d6fe0af2f6e
kaito3desuyo <admin@seapolis.me>
2023-12-15 23:50:44 +0900

    refactor: :wastebasket: 不要なコード・ライブラリを削除する

--- src/core/modules/auth/auth.module.ts
+++ src/core/modules/auth/auth.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { PassportModule } from '@nestjs/passport';
 import { AuthGuard } from './auth.guard';
-import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-    imports: [PassportModule],
-    providers: [AuthGuard, JwtStrategy],
+    providers: [AuthGuard],
 })
 export class AuthModule {}
