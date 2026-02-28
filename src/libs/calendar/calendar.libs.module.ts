7d0aaa6b28095e435660fad40949dc8a686d33ae
kaito3desuyo <admin@seapolis.me>
2026-02-28 19:15:08 +0900

    feat: :sparkles: カレンダー情報のv3エンドポイントを実装する
    - 日付を指定してカレンダー情報を取得する

--- src/libs/calendar/calendar.libs.module.ts
+++ src/libs/calendar/calendar.libs.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/core/modules/auth/auth.module';
 import { CalendarModel } from './infrastructure/models/calendar.model';
 import { CalendarQuery } from './infrastructure/queries/calendar.query';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([CalendarModel]), AuthModule],
+    imports: [TypeOrmModule.forFeature([CalendarModel])],
     exports: [CalendarQuery],
     providers: [CalendarQuery],
 })
--- src/libs/calendar/infrastructure/queries/calendar.query.ts
+++ src/libs/calendar/infrastructure/queries/calendar.query.ts
@@ -2,10 +2,26 @@ import { CrudRequest, GetManyDefaultResponse } from '@dataui/crud';
 import { TypeOrmCrudService } from '@dataui/crud-typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import dayjs from 'dayjs';
 import { isArray } from 'lodash';
-import { Repository } from 'typeorm';
+import {
+    getDayOfWeek,
+    isHoliday,
+    isNewYear,
+    isSpecialCalendarAvailable,
+} from 'src/core/utils/day-of-week';
+import {
+    IsNull,
+    LessThanOrEqual,
+    MoreThanOrEqual,
+    Or,
+    Repository,
+} from 'typeorm';
 import { CalendarDetailsDto } from '../../usecase/dtos/calendar-details.dto';
-import { buildCalendarDetailsDto } from '../builders/calendar-dto.builder';
+import {
+    buildCalendarDetailsDto,
+    CalendarDtoBuilder,
+} from '../builders/calendar-dto.builder';
 import { CalendarModel } from '../models/calendar.model';
 
 @Injectable()
@@ -43,5 +59,40 @@ export class CalendarQuery extends TypeOrmCrudService<CalendarModel> {
         }
 
         return buildCalendarDetailsDto(model);
+    }
+
+    async findOneBySpecificDate(params: {
+        date: string;
+    }): Promise<CalendarDetailsDto> {
+        const { date } = params;
+
+        const format = 'YYYY-MM-DD';
+        const dateInstance = dayjs(date, format);
+        const dateString = dateInstance.format(format);
+
+        const result = await this.calendarRepository.findOne({
+            where: {
+                startDate: Or(LessThanOrEqual(dateString), IsNull()),
+                endDate: Or(MoreThanOrEqual(dateString), IsNull()),
+                ...(isSpecialCalendarAvailable(dateString, format)
+                    ? {
+                          sunday: false,
+                          monday: false,
+                          tuesday: false,
+                          wednesday: false,
+                          thursday: false,
+                          friday: false,
+                          saturday: false,
+                      }
+                    : isHoliday(dateString, format) ||
+                        isNewYear(dateString, format)
+                      ? { sunday: true }
+                      : {
+                            [getDayOfWeek(dateString, format)]: true,
+                        }),
+            },
+        });
+
+        return CalendarDtoBuilder.toDetailsDto(result);
     }
 }
