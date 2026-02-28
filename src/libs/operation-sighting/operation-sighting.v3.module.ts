f487d1b2fa4fea5cd7d1f327bd2f9724574460f9
kaito3desuyo <admin@seapolis.me>
2026-02-28 19:20:00 +0900

    feat: :sparkles: 運用目撃情報のv3エンドポイントを実装する
    - 運用番号ごとの時間断面情報を取得する
    - 編成情報ごとの時間断面情報を取得する

--- src/libs/operation-sighting/operation-sighting.v3.module.ts
+++ src/libs/operation-sighting/operation-sighting.v3.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from 'src/core/modules/auth/auth.module';
+import { FormationLibsModule } from '../formation/formation.libs.module';
+import { OperationLibsModule } from '../operation/operation.libs.module';
 import { OperationSightingLibsModule } from './operation-sighting.libs.module';
 import { OperationSightingV3Controller } from './presentation/operation-sighting.v3.controller';
 import { OperationSightingV3Service } from './usecase/operation-sighting.v3.service';
+import { CalendarLibsModule } from '../calendar/calendar.libs.module';
 
 @Module({
-    imports: [AuthModule, OperationSightingLibsModule],
+    imports: [
+        OperationSightingLibsModule,
+        CalendarLibsModule,
+        OperationLibsModule,
+        FormationLibsModule,
+    ],
     controllers: [OperationSightingV3Controller],
     providers: [OperationSightingV3Service],
 })
