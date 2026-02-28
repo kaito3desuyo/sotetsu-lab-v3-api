559bf15643187b6f3573d3c6fd8bfdaf0defc58f
kaito3desuyo <admin@seapolis.me>
2026-02-28 19:16:08 +0900

    feat: :sparkles: 編成情報のv3エンドポイントを実装する
    - 開始日と終了日を指定して編成情報を取得する

--- src/libs/formation/formation.libs.module.ts
+++ src/libs/formation/formation.libs.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AuthModule } from 'src/core/modules/auth/auth.module';
 import { FormationModel } from './infrastructure/models/formation.model';
 import { VehicleFormationModel } from './infrastructure/models/vehicle-formation.model';
 import { FormationQuery } from './infrastructure/queries/formation.query';
@@ -8,7 +7,6 @@ import { FormationQuery } from './infrastructure/queries/formation.query';
 @Module({
     imports: [
         TypeOrmModule.forFeature([FormationModel, VehicleFormationModel]),
-        AuthModule,
     ],
     exports: [FormationQuery],
     providers: [FormationQuery],
